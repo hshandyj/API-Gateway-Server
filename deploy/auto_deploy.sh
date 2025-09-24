5c8e63f1880650888e7d7f01ffa79cac3de52589
hshandyj <1911420535@qq.com>
2025-09-24 15:44:33 +0800

    update code

--- deploy/auto_deploy.sh
+++ deploy/auto_deploy.sh
@@ -7,7 +7,7 @@ echo "使用版本号: $VERSION"
 
 echo "部署Kubernetes..."
 # 使用sed替换YAML文件中的版本号
-sed -i "s|heshihao0720/HPCP-P/API-Gateway-Server:.*|heshihao0720/HPCP-P/API-Gateway-Server:$VERSION|g" create_test_k8s.yaml
+sed -i "s|heshihao0720/hpcp-p/api-gateway-server:.*|heshihao0720/hpcp-p/api-gateway-server:$VERSION|g" create_test_k8s.yaml
 
 # 应用Kubernetes配置
 kubectl apply -f create_test_k8s.yaml
